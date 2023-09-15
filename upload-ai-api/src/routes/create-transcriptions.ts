1c0c45ba9961a6e5b1b834a733547e9894854dc4
william <william.moreschi@guiadomarceneiro.net>
2023-09-15 12:58:38 -0300

    back-end da aplicação concluído com sucesso

---
+++ upload-ai-api/src/routes/create-transcriptions.ts
@@ -0,0 +1,54 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { createReadStream } from 'node:fs'
+import { prisma } from '../lib/prisma'
+import { openai } from '../lib/openai' 
+
+
+export async function createTranscriptions(app: FastifyInstance) {
+  app.post('/videos/:videoId/transcription', async (request,reply) => {
+    const paramsScheme = z.object({
+      videoId: z.string().uuid(),
+    })
+
+    const { videoId } = paramsScheme.parse(request.params)
+
+    const bodySchema = z.object({
+      prompt: z.string(),
+    })
+
+    const { prompt } = bodySchema.parse(request.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId
+      }
+    })
+
+    const videoPath = video.path
+
+    const audioReadStream = createReadStream(videoPath);
+
+    const response  = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt,
+    })
+
+    const transcription = response.text
+
+    await prisma.video.update({
+      where:{
+        id: videoId
+      },
+      data: {
+        transcription
+      }
+    })
+
+    return { transcription }
+  })
+}
---
+++ upload-ai-api/src/server.ts
@@ -0,0 +1,24 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideos } from './routes/upload-videos';
+import { createTranscriptions } from './routes/create-transcriptions';
+import { generateAiCompletion } from './routes/generate-ai-completion';
+
+export const app = fastify();
+
+app.register(fastifyCors,{
+  origin: '*'
+})
+app.register(getAllPromptsRoute)
+app.register(uploadVideos)
+app.register(createTranscriptions)
+app.register(generateAiCompletion)
+
+
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running...🚀')
+})
